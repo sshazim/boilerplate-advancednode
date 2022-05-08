6e0ee15a04f76e3b0ea2d7fc6d571310fd1a1316
sshazim <sshazim4u@gmail.com>
2022-05-08 17:56:17 +0530

    Updated filename

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'pug')
 
 app.route('/').get((req, res) => {
-  res.render('/pug/index');
+  res.render('/pug');
 });
 
 const PORT = process.env.PORT || 3000;
