6ed0211528bbb987088996b44c0df2ca9023c646
sshazim <sshazim4u@gmail.com>
2022-05-21 22:03:00 +0530

    Changes for serialization & deserialization

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 const session = require('express-session');
 const passport = require('passport');
+const ObjectID = require('mongodb').ObjectID;
 
 const app = express();
 app.set('view engine', 'pug')
@@ -31,4 +32,14 @@ app.route('/').get((req, res) => {
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log('Listening on port ' + PORT);
+});
+
+passport.serializeUser((user, done) => {
+  done(null, user._id);
+});
+
+passport.deserializeUser((id, done) => {
+  myDataBase.findOne({_id: new ObjectID(id)}, (err, doc) => {
+    done(null, null);
+  });
 });
